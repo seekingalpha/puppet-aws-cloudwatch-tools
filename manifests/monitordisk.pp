41e49869cd4a90c4bde14e36cc5eced92b9dbc0b
Yarden Bar <ayash.jorden@gmail.com>
2016-11-24 12:18:08 +0200

    MonitorDisk: use title in cron file name

--- manifests/monitordisk.pp
+++ manifests/monitordisk.pp
@@ -8,7 +8,7 @@ define awscloudwatchtools::monitordisk (
   $monitordisk_command = "${awscloudwatchtools::scripts_dir}/mon-put-instance-data.pl --disk-space-util --disk-path=${disk_path} --from-cron ${awscloudwatchtools::aws_cli_config_file}"
   $cron_time = "*/${frequency_minutes} * * * *"
 
-  file { "/etc/cron.d/monitor_${disk_path}":
+  file { "/etc/cron.d/monitor_${title}":
     content => "${cron_time} root ${monitordisk_command}",
     mode    => '0644',
     require => [
